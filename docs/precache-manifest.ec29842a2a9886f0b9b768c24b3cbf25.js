271af48ee5af620d2119016174655b8df9eaae05
Oscar Mr <oscarmenacho23@gmail.com>
2020-11-07 12:23:34 -0500

    docs agregado

---
+++ docs/precache-manifest.ec29842a2a9886f0b9b768c24b3cbf25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83eb094141137a4b4c89cf72054504ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3d63e74ba26d904ed86",
+    "url": "/static/css/main.53804f9a.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d63e74ba26d904ed86",
+    "url": "/static/js/main.a7bdc555.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
